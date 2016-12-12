c4c65f9ce85eb2c1643d018203776f78137480a2
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-11 19:48:55 -0500

    advanced models

--- books/models.py
+++ books/models.py
@@ -22,11 +22,21 @@ class Author(models.Model):
     def __str__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
+class DahlBookManager(models.Manager):
+    def get_queryset(self):
+        return super(DahlBookManager, self).get_queryset().filter(author='Roald Dahl')
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField(blank=True,null=True)
+    objects = BookManager()
+    dahl_objects = DahlBookManager()
 
     def __str__(self):
         return self.title
